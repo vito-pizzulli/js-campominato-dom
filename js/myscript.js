56900dd5e68c641b01cad44090586a2678577b3a
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-24 13:19:26 +0200

    modified function to adapt it to minefield

--- js/myscript.js
+++ js/myscript.js
@@ -3,6 +3,7 @@ const gridWrapper = document.querySelector('div.grid-wrapper');
 const difficultySelect = document.querySelector('select');
 const playButtonAudio = document.getElementById('play-button-audio');
 const cellClickAudio = document.getElementById('cell-click-audio');
+let bombNumbers;
 
 playButton.addEventListener('click', function () {
     
@@ -13,54 +14,57 @@ playButton.addEventListener('click', function () {
     difficulty = difficultySelect.value;
 
     if (difficulty == 1) {
-        divGenerator (100, 'grid-cell', 'difficulty-1-cell-width', 'clicked-cell', cellClickAudio, gridWrapper);
-        randomNumbersGenerator(1, 100, 16);
+        bombNumbers = randomNumbersGenerator(1, 100, 16);
+        minefieldGenerator (100, 'grid-cell', 'difficulty-1-cell-width', 'clicked-cell', cellClickAudio, gridWrapper, bombNumbers);
 
     } else if (difficulty == 2) {
-        divGenerator (81, 'grid-cell', 'difficulty-2-cell-width', 'clicked-cell', cellClickAudio, gridWrapper);
-        randomNumbersGenerator(1, 81, 16);
+        bombNumbers = randomNumbersGenerator(1, 81, 16);
+        minefieldGenerator (81, 'grid-cell', 'difficulty-2-cell-width', 'clicked-cell', cellClickAudio, gridWrapper, bombNumbers);
 
     } else {
-        divGenerator (49, 'grid-cell', 'difficulty-3-cell-width', 'clicked-cell', cellClickAudio, gridWrapper);
-        randomNumbersGenerator(1, 49, 16);
+        bombNumbers = randomNumbersGenerator(1, 49, 16);
+        minefieldGenerator (49, 'grid-cell', 'difficulty-3-cell-width', 'clicked-cell', cellClickAudio, gridWrapper, bombNumbers);
     }
+
+    console.log(bombNumbers);
 })
 
 
 /* FUNCTIONS */
 
-/**
- * This function generates a set number of empty divs inside a container, applies 2 css classes to them, plus an additional class that is applied only when clicking on the div. The index (i) number will be added inside each div, from 1 to the last one. When clicking a div, the index (i) number will be printed into the console and, if it hasn't been already clicked before, it will play a sound.
- * @param {*} divNumber The number of the divs you want to generate.
- * @param {*} firstClass The name of the first class that will be added to the created div.
- * @param {*} secondClass The name of the second class that will be added to the created div.
- * @param {*} clickClass The name of the class that will be added to the div only when clicking on him.
- * * @param {*} clickSound The sound that will be played when clicking a div that has not already been clicked.
- *  * @param {*} divContainer The container inside which the divs will be created.
+/**This function creates a set number of cells inside a container. Each cell will have a class that defines its style and one that defines its size. By clicking on one of the cells, a further class will be added and, if this has not already been clicked before, a sound will be played. Some of the cells will have bombs inside, they are defined from a set array of numbers.
+ * 
+ * @param {*} cellNumber The number of cells you want to generate.
+ * @param {*} cellStyle The name of the class that defines the cell styling.
+ * @param {*} cellSize The name of the class that defines the cell size.
+ * @param {*} cellClick The name of the class that will be added to the cell only when clicking it.
+ * * @param {*} cellSound The sound that will be played when clicking a cell that has not already been clicked.
+ *  * @param {*} cellWrapper The container inside which the cells will be created.
+ *  * @param {*} cellBombs The array that contains the numbers of the cells that have a bomb inside them.
  */
-function divGenerator (divNumber, firstClass, secondClass, clickClass, clickSound, divContainer) {
+function minefieldGenerator (cellNumber, cellStyle, cellSize, cellClick, cellSound, cellWrapper, cellBombs) {
 
-    for (let i = 1; i <= divNumber; i++) {
+    for (let i = 1; i <= cellNumber; i++) {
             const cell = document.createElement('div');
             /* cell.innerHTML = [i]; */
-            cell.classList.add(firstClass, secondClass);
+            cell.classList.add(cellStyle, cellSize);
             
             cell.addEventListener('click', function () {
 
-                if (!cell.classList.contains(clickClass)) {
-                    clickSound.load();
-                    clickSound.play();
+                if (!cell.classList.contains(cellClick)) {
+                    cellSound.load();
+                    cellSound.play();
                 }
 
-                cell.classList.add(clickClass);
+                cell.classList.add(cellClick);
                 console.log("Hai cliccato la cella n° " + [i])
             })
-            divContainer.appendChild(cell);
+            cellWrapper.appendChild(cell);
         }
 }
 
-/**
- * This function creates an array of random numbers. You can choose the min and max random number that will be generated, and how many elements will the array have.
+/**This function creates an array of random numbers. You can choose the min and max random number that will be generated, and how many elements will the array have.
+ *
  * @param {*} minNumber The min random number that will be generated.
  * @param {*} maxNumber The max random number that will be generated.
  * @param {*} totalNumbers The number of elements that the array will have.
@@ -81,6 +85,6 @@ function randomNumbersGenerator(minNumber, maxNumber, totalNumbers) {
                 randomNumbersList.push(randomNumber);
             }
         }
-        console.log(randomNumbersList);
     }
+    return randomNumbersList;
 }
